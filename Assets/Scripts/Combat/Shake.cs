1e909649c0f9147b261e58909ec98f4ac9f44eda
PurpleGita <rasmus.munk01@gmail.com>
2026-02-22 19:29:54 +0100

    shakes
    shakes

--- Assets/Scripts/Combat/Shake.cs
+++ Assets/Scripts/Combat/Shake.cs
@@ -5,21 +5,15 @@ public class Shake : MonoBehaviour
 {
 
     [SerializeField]
-    AnimationCurve curve;
+    AnimationCurve _curve;
 
     public void Shaking(float duration, float intensity) 
     {
         StartCoroutine(ShakingLocal(duration, intensity));
     }
 
-    /// <summary>
-    /// Coroutine that applies a shaking effect to the GameObject's position for a given duration and intensity.
-    /// The shake strength is modulated over time using the provided AnimationCurve.
-    /// At the end of the shake, the GameObject's position is reset to its original value.
-    /// </summary>
-    /// <param name="duration">How long the shake should last, in seconds.</param>
-    /// <param name="intensity">The maximum strength of the shake.</param>
-    /// <returns>IEnumerator for coroutine execution.</returns>
+
+    /// Corutine der ryster kamaret.
     public IEnumerator ShakingLocal(float duration,float intensity) 
     { 
         Vector3 startPosition = transform.position;
@@ -28,7 +22,7 @@ public class Shake : MonoBehaviour
         while (elapsedTime < duration)
         {
             elapsedTime += Time.deltaTime;
-            float strength = curve.Evaluate(elapsedTime / duration) * intensity;
+            float strength = _curve.Evaluate(elapsedTime / duration) * intensity;
             transform.position = startPosition + Random.insideUnitSphere * strength;
             yield return null;
         }
